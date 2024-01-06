7ca3cff32f07b61c261df1e3cd2f2cdd364aaf1f
ziagham <amin.ziagham@gmail.com>
2024-01-07 00:01:43 +0100

    Add new project to add flowsynx client in order to use in the AspNet Core applications

---
+++ src/FlowSynx.AspNetCore/FlowSynxServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FlowSynx.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace FlowSynx.AspNetCore;
+
+public static class FlowSynxServiceCollectionExtensions
+{
+    public static void AddFlowSynxClient(this IServiceCollection services)
+    {
+        if (services is null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        // This pattern prevents registering services multiple times in the case AddFlowSynxClient is called
+        // by non-user-code.
+        if (services.Any(s => s.ImplementationType == typeof(IFlowSynxClient)))
+            return;
+
+        services.TryAddSingleton<IFlowSynxClient>(_ =>
+        {
+            var builder = new FlowSynxClientFactory();
+            return builder.CreateClient();
+        });
+    }
+}
