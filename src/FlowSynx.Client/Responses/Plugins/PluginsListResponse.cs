7b0750a6b8ab70c60158a998e922556009860d6c
ziagham <amin.ziagham@outlook.com>
2024-07-31 23:24:42 +0200

    Add PluginDetailsSpecification to the PluginDetails response object #8

--- src/FlowSynx.Client/Responses/Plugins/PluginsListResponse.cs
+++ src/FlowSynx.Client/Responses/Plugins/PluginsListResponse.cs
@@ -5,4 +5,6 @@ public class PluginsListResponse
     public required Guid Id { get; set; }
     public required string Type { get; set; }
     public string? Description { get; set; }
+    public List<PluginDetailsSpecification>? Specifications { get; set; } = new List<PluginDetailsSpecification>();
+
 }
