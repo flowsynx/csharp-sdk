6a95633edeb0aae8ddf80795da1a3239b5f744c4
ziagham <amin.ziagham@outlook.com>
2024-08-11 16:48:01 +0200

    Refactoring Client to listing plugins based on filters #11

--- src/FlowSynx.Client/Requests/Plugins/PluginsListRequest.cs
+++ src/FlowSynx.Client/Requests/Plugins/PluginsListRequest.cs
@@ -2,5 +2,9 @@
 
 public class PluginsListRequest
 {
-    public string? Namespace { get; set; } = string.Empty;
+    public string? Include { get; set; }
+    public string? Exclude { get; set; }
+    public bool? CaseSensitive { get; set; } = false;
+    public string? MaxResults { get; set; }
+    public string? Sorting { get; set; }
 }
