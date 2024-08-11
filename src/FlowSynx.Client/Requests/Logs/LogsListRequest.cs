082cc5d64c279a5bdcf8e3553e2a724cc4683983
ziagham <amin.ziagham@outlook.com>
2024-08-11 16:48:19 +0200

    Refactoring Client to listing Logs based on filters #12

--- src/FlowSynx.Client/Requests/Logs/LogsListRequest.cs
+++ src/FlowSynx.Client/Requests/Logs/LogsListRequest.cs
@@ -2,7 +2,12 @@
 
 public class LogsListRequest
 {
+    public string? Include { get; set; }
+    public string? Exclude { get; set; }
     public string? MinAge { get; set; }
     public string? MaxAge { get; set; }
     public string? Level { get; set; }
+    public bool? CaseSensitive { get; set; } = false;
+    public string? MaxResults { get; set; }
+    public string? Sorting { get; set; }
 }
