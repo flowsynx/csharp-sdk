1661ccedf1c8cb6537e96342eb5caa23173e2be4
ziagham <amin.ziagham@outlook.com>
2024-09-07 22:13:47 +0200

    Add StatusCode to the HttpResult #17

--- src/FlowSynx.Client/Responses/HttpResult.cs
+++ src/FlowSynx.Client/Responses/HttpResult.cs
@@ -1,7 +1,10 @@
-﻿namespace FlowSynx.Client.Responses;
+﻿using System.Net;
+
+namespace FlowSynx.Client.Responses;
 
 public class HttpResult<TResult>
 {
+    public int StatusCode { get; set; }
     public IEnumerable<KeyValuePair<string, IEnumerable<string>>> Headers { get; set; } = new List<KeyValuePair<string, IEnumerable<string>>>();
     public required TResult Payload { get; set; }
 }
