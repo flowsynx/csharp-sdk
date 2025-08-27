1b4408a1a997e8a6e4d0a1f17cd3c3917589eddb
ziagham <amin.ziagham@outlook.com>
2025-08-27 22:35:29 +0200

    Add version property to the response #46

--- src/FlowSynx.Client/Messages/Responses/PluginConfig/PluginConfigDetailsResponse.cs
+++ src/FlowSynx.Client/Messages/Responses/PluginConfig/PluginConfigDetailsResponse.cs
@@ -5,5 +5,6 @@ public class PluginConfigDetailsResponse
     public required Guid Id { get; set; }
     public required string Name { get; set; }
     public required string Type { get; set; }
+    public required string Version { get; set; }
     public Dictionary<string, string?>? Specifications { get; set; }
 }
